6b28679898458801124e27b6d18117d8dfbf3c4b
Brian Hart <softwareguycoder@gmail.com>
2018-09-24 09:20:42 -0400

    24 Sep 2018 Latest updates to client and server

--- client/client.c
+++ client/client.c
@@ -30,6 +30,21 @@
 #define MAX_LINE_LENGTH 255     // The maximum length of a line
 #define USAGE_STRING	"Usage: client <hostname> <port_num>\n" 	// Usage string
 
+#define RECV_BLOCK_SIZE	1
+#define RECV_FLAGS	0
+
+// Frees the memory at the address specified.
+// pBuffer is the address of a pointer which points to memory
+// allocated with the *alloc functions (malloc, calloc, realloc)
+void free_buffer(void** pBuffer)
+{   
+    if (pBuffer == NULL || *pBuffer == NULL)
+        return;     // Nothing to do since there is no address referenced
+    
+    free(*pBuffer);
+    *pBuffer = NULL;
+}
+
 void error_and_close(int socket, const char* msg) 
 {     
     fprintf(stderr, "%s", msg);
@@ -50,6 +65,95 @@ void error(const char* msg)
     fprintf(stderr, "%s", msg);
 	perror("client");
 	exit(ERROR);
+}
+
+// This function gets a line of text from a socket,
+// and stores it in location referenced by lineptr.
+// Returns the total bytes read of the current line. total_read
+// points to an integer variable that will receive the 
+// OVERALL total bytes read.
+int get_line(int socket, char **lineptr, int *total_read) 
+{
+	int bytes_read = 0;
+	int bytes_of_this_line = 0;
+	
+	if (lineptr == NULL
+		|| socket <= 0
+		|| total_read == NULL)
+	{
+		fprintf(stderr, "server: invalid input to getline.\n");	
+		exit(ERROR);
+	}
+
+	if (*lineptr == NULL) 
+	{
+		// If we get a NULL for *lineptr, just allocate up some
+		// brand-new storage of size RECV_BLOCK_SIZE plus an extra 
+		// slot to hold the null-terminator
+		*total_read = 0;
+		*lineptr = (char*)calloc(RECV_BLOCK_SIZE + 1, sizeof(char));
+	}
+	
+    char prevch = '\0';
+	while(1) 
+	{
+		char ch;		// receive one char at a time
+		bytes_read = recv(socket, &ch, RECV_BLOCK_SIZE, RECV_FLAGS);
+		if (bytes_read <= 0 || prevch == '.') 
+		{
+			// Connection terminated, so stop the loop
+			// but continue running the program (because we may need
+			// to free up storage)
+			if (bytes_read < 0)
+				fprintf(stderr, 
+					"server: Network error stopped us from receiving more text.");
+
+            prevch = ch;
+			break;
+		}
+
+		// If we are here, then stuff came over the wire
+		// stick the character received, from ch, into the next 
+		// storage element referenced by *lineptr + total_read
+		// and then allocate some more memory to hold the 
+		// next char and then the null terminator
+		*(*lineptr + *total_read) = ch;
+
+		// Tally the total bytes read overall
+		*total_read += bytes_read;
+		bytes_of_this_line += bytes_read;
+
+		// If the newline ('\n') character was the char received,
+		// then we're done, time to apply the null terminator
+		if (ch == '\n')
+		{
+            prevch = ch;
+			break;
+		}
+		
+		// re-allocate more memory and make sure to leave room 
+		// for the null-terminator
+		*lineptr = (char*)realloc(*lineptr, (*total_read + RECV_BLOCK_SIZE + 1)*sizeof(char));
+	}
+
+	// We are done receiving, cap the string off with a null terminator
+	// after resizing the buffer to match the total bytes read + 1.  if
+	// a connection error happened prior to reading even one byte, then
+	// total_read will be zero and the call below will be equivalent to
+	// free.  strlen(*lineptr) will then return zero, and this will be
+	// how we can tell not to call free() again on *lineptr
+	*lineptr = (char*)realloc(*lineptr, (*total_read + 1)*sizeof(char));
+
+	if (total_read > 0)
+	{
+		*(*lineptr + *total_read) = '\0';	// cap the buffer off with the null-terminator
+	}
+
+	// Now the storage at address *lineptr should contain the entire 
+	// line just received, plus the newline and the null-terminator, plus
+	// any previously-received data
+	
+	return bytes_of_this_line;
 }
 
 int main(int argc, char* argv[])
@@ -81,12 +185,20 @@ int main(int argc, char* argv[])
     fprintf(stdout, 
         "client: Configured to connect to server at address '%s'.\n", hostname);
     fprintf(stdout,
-        "client: Configured to connect to server listening on port %d.\n", port);
+        "client: Configured to connect to server listening on port %d.\n", port);  
+
+    /* Just in case the user has entered a host name, such as 'www.microsoft.com'
+        or 'localhost' instead of an IP address, resolve the hostname using DNS
+        in order to attempt to map it to the corresponding IP address for passing
+        to the connect() function later on down the line.  Put this attempt to
+        resolve the name code before the creation of the socket endpoint, so if
+        we fail to resolve the name, then we have not wasted operating system resources
+        by creating a new socket that we now do not need. */
 
     fprintf(stdout, 
         "client: Resolving hostname '%s'...\n", hostname);
 
-   if ( (he = gethostbyname(hostname) ) == NULL ) {
+    if ( (he = gethostbyname(hostname) ) == NULL ) {
         error("client: Hostname resolution failed.\n");
     }
 
@@ -130,11 +242,12 @@ int main(int argc, char* argv[])
 
     /* Show a '>' prompt to the user.  If the user just presses ENTER at a 
        prompt, then just give the user a new prompt.  If the user enters the
-       words 'exit' or 'quit', then exit this program.  Otherwise, send whatever
-       string(s) the user types at the '>' prompt to the server, and then display
-       the server's response, if any.
+       words 'exit' or 'quit' at the '>' prompt, then exit this program.  Otherwise, 
+       send whatever string(s) the user types at the '>' prompt to the server, 
+       and then display the server's response, if any.
     */
 
+    int total_read = 0;             // total reply bytes read from the server
     int total_entered = 0;          // total bytes typed by the user for the current message
 
     fprintf(stdout, "> ");
@@ -150,17 +263,40 @@ int main(int argc, char* argv[])
             continue;
         }
         
+        // Keep a running total of the total bytes entered
+        total_entered += strlen(cur_line);
+
         // send the text just now entered by the user to the server
         if( send(client_socket , cur_line, strlen(cur_line) , 0) < 0)
         {
             error_and_close(client_socket, "client: Failed to send the data.\n");
-            return 1;
+            return ERROR;
+        } 
+
+        // Now, assume the server has sent a reply, and call the recv() function
+        // to attempt to pull the text sent back by the server off of the data
+        // stream.  Assume that the server just sends back one line at a time.
+        char *reply_buffer = NULL;
+
+        if (0 > get_line(client_socket, &reply_buffer, &total_read))
+        {
+            free_buffer((void**)&reply_buffer);
+            error_and_close(client_socket, "client: Failed to receive the line of text back from the server.\n");
+            return ERROR;            
+        }
+        else
+        {
+            // Print the line received from the server to the console with a
+            // 'S: ' prefix in front of it.  We assume that the reply_buffer
+            // contains the newline character.  Free the memory allocated for
+            // the server reply.
+            fprintf(stdout, "S: %s", reply_buffer);
+        
+            free_buffer((void**)&reply_buffer);
         }
 
         fprintf(stdout, "> ");
     }
-
-    // TODO: Add code here to connect to the server
 
     // TODO: Add code here to provide a user interface for sending
     // data to the server.  Be sure to prompt the user to type a period ('.')
