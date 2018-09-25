72685f5708bda10c552a466a4f3b39e839fd3e92
Brian Hart <softwareguycoder@gmail.com>
2018-09-25 10:33:43 -0400

    25 Sep 2018 Updated build shell scripts to echo the steps

--- client/build.sh
+++ client/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo Deleting outputs...
 rm -rf client
+echo Building client.c...
 gcc client.c -o client
 echo Build complete!
--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo Deleting outputs...
 rm -rf server
+echo Building server.c...
 gcc server.c -o server
 echo Build complete!
